00b9ec1796a3a85f65bf656d7e89ccfdf74ad2cc
kevin-martins <kev.martinsvieira@gmail.com>
2023-05-22 00:23:46 +0200

    feat: adding product data fetching

--- services/product.ts
+++ services/product.ts
@@ -1,23 +1,51 @@
+import { ProductProps, ProductResponseProps } from "@/models/product";
 import request, { gql } from "graphql-request";
 
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
-export const getProducts = async (): Promise<any> => {
+export const getProducts = async (): Promise<ProductProps[]> => {
   const query = gql`
     query Assets {
       products {
         id
         name
+        image {
+          url
+        }
         price
-        slug
-        description
-        reviews {
+        comments {
           rating
         }
       }
     }
   `
+  const result: ProductResponseProps = await request(graphqlAPI!, query);
+
+  return result.products;
+}
+
+export const getProductDetail = async (id: string): Promise<any> => {
+  const query = gql`
+    query Assets {
+      product (where: id) {
+        id
+        name
+        price
+        slug
+        createdAt
+        publishedAt
+        updatedAt
+        comments {
+          user
+          title
+          rating
+          description
+          publishedAt
+        }
+      }
+    }
+  `
   const result: any = await request(graphqlAPI!, query);
-  console.log("res", result.products)
+
   return result.products;
 }
