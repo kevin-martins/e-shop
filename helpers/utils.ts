90b6545ede59c8e88f7ab850857c7262d941e319
kevin-martins <kev.martinsvieira@gmail.com>
2023-05-22 00:24:32 +0200

    feat: shared functions

--- helpers/utils.ts
+++ helpers/utils.ts
@@ -1,4 +1,11 @@
 export const average = (ratings: number[]): number => {
-  const somme = ratings.reduce((acc, curr) => curr += acc, 0)
-  return somme / ratings.length
+  return ratings.reduce((acc, curr) => curr += acc, 0) / ratings.length
+}
+
+export const normalisePrice = (price: number): number => {
+  return price / 100 
+}
+
+export const priceToEuro = (price: number): string => {
+  return price.toString().replace('.', ',')
 }
